8652d0a356c424359488a7aee418f9fb465a2749
jwilson42 <wilson.jd022@gmail.com>
2021-04-12 09:38:38 -0700

    changed home layout

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,13 @@
-import Fade from 'react-bootstrap/Fade'
+import Fade from 'react-bootstrap/Fade';
+import Image from 'react-bootstrap/Image'
+import { useState } from 'react';
+import './home.css';
 
 const Home = () => {
 
-  const [open, setOpen] = useState()
-
   return (
-    <div className="bg-light">
-      <Fade in={true}>
-        <div  id="example-fade-text">
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
-        </div>
-      </Fade>
+    <div id="HomeContainer" className="bg-transparent text-center position-absolute top-50 start-50 translate-middle">
+      <Image src="https://res.cloudinary.com/dom5vocai/image/upload/v1615930252/new_logo_bwgzbl.png" rounded />
     </div>
   )
 }
